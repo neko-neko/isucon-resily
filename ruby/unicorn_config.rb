b14ac50f2a190efdefac2cf2844711c8b5497ba3
root <root@ip-172-31-5-180.ap-northeast-1.compute.internal>
2023-04-19 04:37:03 +0000

    mod unicorn

--- ruby/unicorn_config.rb
+++ ruby/unicorn_config.rb
@@ -1,3 +1,7 @@
-worker_processes 1
+worker_processes 48
+
 preload_app true
+
+#listen "/home/isucon/private_isu/webapp/public/unicorn.sock"
+pid "/home/isucon/private_isu/webapp/public/unicorn.pid"
 listen "0.0.0.0:8080"
